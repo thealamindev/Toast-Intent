16ce60d011716cfb2dce80fa1fbe5ae820f9bd61
Al-Amin <37735466+al-amin-dev@users.noreply.github.com>
2021-08-19 15:58:10 +0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -15,11 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         main_one = findViewById(R.id.main_one);
+        
         main_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "GO", Toast.LENGTH_SHORT).show();
+                
                 Intent intent = new Intent(MainActivity.this, View_one.class);
                 startActivity(intent);
             }

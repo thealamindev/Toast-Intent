f8531c3bd9e4ea6a767bff78bd0832d3ea0f1b6c
Al-Amin <37735466+al-amin-dev@users.noreply.github.com>
2021-08-19 15:56:37 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView main_one, main_two, main_three, main_four;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        main_one = findViewById(R.id.main_one);
+        main_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "GO", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, View_one.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
